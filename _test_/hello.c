1ebdf853d17687c6757b01ad0ada786b1c723084
moweida <2594041017@qq.com>
2023-04-08 23:35:33 +0800

    add a test directory.

---
+++ _test_/hello.c
@@ -0,0 +1,25 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2023 NULL
+ *                  All rights reserved.
+ *
+ *       Filename:  hello.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(2023年04月07日)
+ *         Author:  MoWeida <2594041017@qq.com>
+ *      ChangeLog:  1, Release initial version on "2023年04月07日 18时55分31秒"
+ *                 
+ ********************************************************************************/
+
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hello linux!\n");
+	return 0;
+}
+
+
+
